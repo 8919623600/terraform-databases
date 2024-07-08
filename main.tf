8058d7936531490decf19a298878d006acfedd84
Jyoti sahoo <jyoti29sahoo@gmail.com>
2024-07-08 12:03:43 +0530

    updated

--- main.tf
+++ main.tf
@@ -11,37 +11,37 @@ module "docdb" {
 }
 
 
-module "redis" {
-    source                    = "./vendor/modules/redis/"       # Terrafile clones the backend module mentioned in the Terrafile and keeps it local to the root 
-    ENV                       = var.ENV
-    NUMBER_OF_NODES           = var.NUMBER_OF_NODES
-    PORT                      = var.PORT
-    NODE_TYPE                 = var.NODE_TYPE
-    REDIS_FAMILY              = var.REDIS_FAMILY
-    ENGINE_VERSION            = var.ENGINE_VERSION
+# module "redis" {
+#     source                    = "./vendor/modules/redis/"       # Terrafile clones the backend module mentioned in the Terrafile and keeps it local to the root 
+#     ENV                       = var.ENV
+#     NUMBER_OF_NODES           = var.NUMBER_OF_NODES
+#     PORT                      = var.PORT
+#     NODE_TYPE                 = var.NODE_TYPE
+#     REDIS_FAMILY              = var.REDIS_FAMILY
+#     ENGINE_VERSION            = var.ENGINE_VERSION
     
 
-}
+# }
 
-module "mysql" {
-    source                    = "./vendor/modules/mysql/"       # Terrafile clones the backend module mentioned in the Terrafile and keeps it local to the root 
-    ENV                       = var.ENV
-    MYSQL_PORT                = var.MYSQL_PORT
-    MYSQL_ENGINE_FAMILY       = var.MYSQL_ENGINE_FAMILY
-    MYSQL_INSTANCE_CLASS      = var.MYSQL_INSTANCE_CLASS
-    MYSQL_STORAGE             = var.MYSQL_STORAGE
-    MYSQL_ENGINE              = var.MYSQL_ENGINE
-    MYSQL_VERSION             = var.MYSQL_VERSION
-    MYSQL_SKIP_FINAL_SNAPSHOT = var.MYSQL_SKIP_FINAL_SNAPSHOT
+# module "mysql" {
+#     source                    = "./vendor/modules/mysql/"       # Terrafile clones the backend module mentioned in the Terrafile and keeps it local to the root 
+#     ENV                       = var.ENV
+#     MYSQL_PORT                = var.MYSQL_PORT
+#     MYSQL_ENGINE_FAMILY       = var.MYSQL_ENGINE_FAMILY
+#     MYSQL_INSTANCE_CLASS      = var.MYSQL_INSTANCE_CLASS
+#     MYSQL_STORAGE             = var.MYSQL_STORAGE
+#     MYSQL_ENGINE              = var.MYSQL_ENGINE
+#     MYSQL_VERSION             = var.MYSQL_VERSION
+#     MYSQL_SKIP_FINAL_SNAPSHOT = var.MYSQL_SKIP_FINAL_SNAPSHOT
     
-}
+# }
 
 
 
-module "rabbitmq" {
-    source                  = "./vendor/modules/rabbitmq/"       # Terrafile clones the backend module mentioned in the Terrafile and keeps it local to the root 
-    ENV                     = var.ENV
-    RABBITMQ_INSTANCE_TYPE  = var.RABBITMQ_INSTANCE_TYPE
-    RABBITMQ_PORT           = var.RABBITMQ_PORT
+# module "rabbitmq" {
+#     source                  = "./vendor/modules/rabbitmq/"       # Terrafile clones the backend module mentioned in the Terrafile and keeps it local to the root 
+#     ENV                     = var.ENV
+#     RABBITMQ_INSTANCE_TYPE  = var.RABBITMQ_INSTANCE_TYPE
+#     RABBITMQ_PORT           = var.RABBITMQ_PORT
     
-}
+# }
